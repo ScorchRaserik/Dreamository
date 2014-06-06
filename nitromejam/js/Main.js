4439634f795dcaa723db0f4d393217e79a328d6d
Ryan Fisher <backupaccount10@gmail.com>
2014-06-06 03:25:47 -0400

    fixed reticule

--- nitromejam/js/Main.js
+++ nitromejam/js/Main.js
@@ -3,8 +3,9 @@ Dream.Main = function(game) {
 	canJump = true;
 	nextFire = 0;
 	fireRate = 100;
-	PLAYER_SCALE = 1.0;
+	PLAYER_SCALE = 0.6;
 	nextCloud = 0;
+	nextMountain = 0;
 	cloudRate = 10000;
 };
 
@@ -12,34 +13,65 @@ Dream.Main.prototype = {
 
 	create: function() {
 
+		// Make the world larger than the boundaries
+		this.game.world.setBounds(0, 0, 2200, 0);
+
 		//Add objects
-		this.add.sprite(0, 0, 'sky');
-		platforms = this.game.add.group();
+		this.add.tileSprite(0, 0, 2200, 550, 'sky');
 		clouds = this.game.add.group();
+		mountains = this.game.add.group();
+		platforms = this.game.add.group();
+
 
 		//Physics setup
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		platforms.enableBody = true;
 		clouds.enableBody = true;
+		mountains.enableBody = true;
+		platforms.enableBody = true;
+
 
 		//Set up clouds
 		clouds.createMultiple(30, 'cloud', 0, false);
 	   	clouds.setAll('outOfBoundsKill', true);
 	   	clouds.setAll('checkWorldBounds', true);
 
-		//Build world
-		ground = platforms.create(0, this.game.world.height - 50, 'ground');
-		ledge1 = platforms.create(0, 400, 'ground');
-		ledge2 = platforms.create(450, 250, 'ground');
-		ground.scale.setTo(4, 2);
-		ground.body.immovable = true;
-		ledge1.body.immovable = true;
-		ledge2.body.immovable = true;
+	   	//Set up mountains
+	   	mountains.createMultiple(30, 'mountain', 0, false);
+	   	mountains.setAll('outOfBoundsKill', true);
+	   	mountains.setAll('checkWorldBounds', true);
 
+	   	//3 clouds spawn at once
+	   	for (var i = 0; i < 18; i++) 
+	    {
+	    	// create a star inside of the 'stars' group
+			var cloudTemp = clouds.create(i * 225, 50 + (Math.random() * 140), 'cloud');
+
+			// let gravity do its thing
+			cloudTemp.body.gravity.x = 0;
+
+			// this gives each star a slightly random bounce value
+			cloudTemp.body.velocity.x = -10 - (Math.random() * 15);
+	    }
+
+	  	//5 mountains spawn at once
+	   	for (var i = 0; i < 30; i++) 
+	    {
+			var mTemp = mountains.create(((i-1) * 120) + (Math.random() * 2), this.game.world.height - 245 + (Math.random() * 60), 'mountain');
+
+			mTemp.body.gravity.x = 0;
+
+			mTemp.body.velocity.x = -45 - (Math.random() * 8);
+	    }	   	
+
+		//Build world
+		ground = platforms.create(0, this.game.world.height - 45, 'ground');
+		ground.body.immovable = true;
 
 		//Set up player
-		player = this.add.sprite(32, this.game.world.height - 100, 'player');
+		player = this.add.sprite(226, this.game.world.height - 100, 'player');
+		player.anchor.setTo(0.5, 0.5);
 		this.game.physics.arcade.enable(player);
+		this.game.camera.follow(player, Phaser.Camera.FOLLOW_PLATFORMER);
 		player.body.gravity.y = 5000 * PLAYER_SCALE;
 		player.body.collideWorldBounds = true;
 		player.body.drag.set((3500 * PLAYER_SCALE), (2000 * PLAYER_SCALE));
@@ -57,6 +89,7 @@ Dream.Main.prototype = {
 
 	    //targetting
 	    target = this.add.sprite(this.game.input.x, this.game.input.y, 'target');
+	    target.fixedToCamera = true;
 
 
 		//Controls
@@ -68,13 +101,19 @@ Dream.Main.prototype = {
 		this.game.physics.arcade.collide(player, platforms);
 
 		//Use reticule for mouse
-		target.x = this.game.input.x - 10;
-		target.y = this.game.input.y - 10;
+		target.cameraOffset.x = this.game.input.x - 22;
+		target.cameraOffset.y = this.game.input.y - 12;
 
 		//Spawn clouds
 		if(this.game.time.now > nextCloud)
 		{
 			this.spawnCloud();
+		}
+
+		//Spawn mountains
+		if(this.game.time.now > nextMountain)
+		{
+			this.spawnMountain();
 		}
 
 
@@ -137,18 +176,28 @@ Dream.Main.prototype = {
 	        nextFire = this.game.time.now + fireRate;
 	        bullet = bullets.getFirstExists(false);
 	        bullet.reset(player.x + player.body.halfWidth, player.y + player.body.halfHeight);
-	        bullet.rotation = this.game.physics.arcade.moveToPointer(bullet, 1200, this.game.input.activePointer);
+	        bullet.rotation = this.game.physics.arcade.moveToPointer(bullet, 1000, this.game.input.activePointer);
 	    }
 	},
 
 	spawnCloud: function() {
 		nextCloud = this.game.time.now + cloudRate;
 		cloud = clouds.getFirstExists(false);
-		cloud.reset(549, 50 + (Math.random() * 140), 'cloud');
+		cloud.reset(2199, 30 + (Math.random() * 120), 'cloud');
 		cloud.body.gravity.x = 0;
-		cloud.body.velocity.x = -10 - (Math.random() * 15);
-		cloudScale = Math.random() + 0.6; 
+		cloud.body.velocity.x = -15 - (Math.random() * 15);
+		cloudScale = Math.random() + .6; 
 		cloud.scale.setTo(cloudScale, cloudScale);
+	},
+
+	spawnMountain: function() {
+		nextMountain = this.game.time.now + (cloudRate-7150);
+		mountain = mountains.getFirstExists(false);
+		mountain.reset(2199, this.game.world.height-250, 'mountain');
+		mountain.body.gravity.x = 0;
+		mountain.body.velocity.x = -45 - (Math.random() * 8);
+		mountainScale = ((Math.random()/2.75) + 1); 
+		mountain.scale.setTo(mountainScale, mountainScale);
 	},
 
 	render: function() {
